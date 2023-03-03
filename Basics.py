4dfa5c0e2b431783e66878916f2f8ddc1e70e51a
fahim al shihab <97816146+fahimalshihab@users.noreply.github.com>
2023-03-04 02:35:23 +0600

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,146 @@
+'''print('hello')
+age = 200
+print(age)
+print(type(age))
+
+INPUT
+name = input("what is your name ? ")
+print("hello " +name)
+
+type convertion
+BY = input("enter your birth year :")
+age = 2020 - int(BY)
+print(age)
+
+cy = int (input("age ?"))
+ag = 10 + cy
+print(ag)
+
+name = "Fahim al shihab"
+print(name.upper())
+print(name.find('m'))
+print(name.find('al'))
+print(name.replace('F','E'))
+print('Fahim' in name)
+
+
+#Arithmetic
+print(10+3)
+print(10/3)
+print(10//3)
+print(10**3)
+print(10*3)
+print(10+3*2)
+print((10+3)*2)
+
+
+#comparison
+x=3==2
+print(x)
+y=3>2
+print(y)
+#logical operators
+price = 100
+print(price > 40 and price > 32)
+print(price > 10 or price < 99)
+
+
+#if
+
+t = 3
+if t > 30:
+    print("hot day")
+    print('yeam')
+elif t > 20 :
+    print("nice day")
+elif t > 31 :
+    print("cool")
+else :
+    print("dead")
+
+print("done")
+
+
+#Ex
+
+w = int(input("weight : "))
+x = str(input(" kg or gram: "))
+
+if x == 'kg' or "kG" :
+    print(w*10)
+elif x == 'gram' or "GRAM":
+    print(w*1000)
+
+
+#while LOOP
+
+i=5
+while i>= 1:
+    print(i*"*")
+    i-=1
+
+
+j=1
+while j<=5:
+    print(j*"*")
+    j+=1
+
+#LIST
+names = ['fahim','al','shihab']
+names[0]="him"
+print(names[-1])
+print(names)
+print(names[0:2])
+
+#LIST METHOD
+
+numbers = [1,2,3,4,5]
+numbers.append(6)
+numbers.insert(0,-1)
+numbers.remove(3)
+#numbers.clear()
+print(len(numbers))
+print(20  in numbers)
+print(numbers)
+
+#FOR LOOPS
+
+numbers = [1,2,3,4,5]
+for i in numbers :
+    print(i)
+
+#range function
+
+numbers = range(5,10)
+print(numbers)
+for n in numbers:
+    print(n)
+
+numbers1 = range(5,10,2)
+print(numbers1)
+for n1 in numbers1:
+    print(n1)
+
+for i in range(5):
+    print(i)
+
+
+#TOUPLE
+
+numbers=(1,2,3,4)
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
